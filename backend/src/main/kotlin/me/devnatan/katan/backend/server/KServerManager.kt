84f2d9474431ec2c364840e0000220267ceba1d0
DevNatan <itroken99@gmail.com>
2019-04-28 13:50:10 -0300

    Fix resources path and server default jar file.

--- backend/src/main/kotlin/me/devnatan/katan/backend/server/KServerManager.kt
+++ backend/src/main/kotlin/me/devnatan/katan/backend/server/KServerManager.kt
@@ -1,15 +1,21 @@
 package me.devnatan.katan.backend.server
 
+import io.ktor.http.cio.websocket.Frame
 import io.netty.util.internal.ConcurrentSet
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.runBlocking
+import me.devnatan.katan.backend.Katan
 import me.devnatan.katan.backend.io.createProcess
+import me.devnatan.katan.backend.util.asJsonString
 import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.io.File
 import java.io.FileNotFoundException
 import java.util.*
 
-class KServerManager(private val logger: Logger) {
+class KServerManager {
 
+    private val logger: Logger = LoggerFactory.getLogger(this::class.simpleName)
     private val servers = ConcurrentSet<KServer>()
 
     fun getServers(): Set<KServer> {
@@ -19,11 +25,12 @@ class KServerManager(private val logger: Logger) {
     fun getServer(id: String): KServer? = servers.find { it.id.equals(id, true) }
 
     fun load(coroutine: CoroutineScope) {
-        val folder = File("C:\\Users\\GFIRE\\Documents\\CloudCraft\\Katan\\backend\\src\\main\\resources\\servers")
+        val folder = File(this::class.java.classLoader.getResource("servers").file)
         if (!folder.exists())
             folder.mkdir()
 
-        for (serverPath in folder.listFiles()) {
+        val folders = folder.listFiles() ?: return
+        for (serverPath in folders) {
             if (!serverPath.isDirectory) continue
 
             val serverId = serverPath.name
@@ -39,12 +46,12 @@ class KServerManager(private val logger: Logger) {
 
             loadServer(coroutine, serverId, serverPath)
         }
-        logger.info("[+] Loaded ${servers.size} servers.")
+        logger.info("Loaded ${servers.size} servers.")
     }
 
     fun loadServer(coroutine: CoroutineScope, id: String, path: File): KServer {
         val jar = path.listFiles().firstOrNull {
-            it.name == "PaperSpigot-1.8.8-R0.1-SNAPSHOT-latest.jar"
+            it.name.endsWith(".jar")
         } ?: throw FileNotFoundException("Couldn't find PaperSpigot for server \"$id\".")
 
         val server = KServer(
@@ -52,7 +59,17 @@ class KServerManager(private val logger: Logger) {
             KServerPath(path.absolutePath, jar.name),
             createProcess(coroutine, path, "java", "-Xms256M", "-Xmx512M", "-jar", jar.name, "-o", "FALSE"),
             EnumKServerState.STOPPED
-        )
+        ).apply {
+            onMessage = { message ->
+                runBlocking {
+                    Katan.webSocket.outgoing.send(Frame.Text(mapOf(
+                        "type" to "server-log",
+                        "server" to id,
+                        "message" to message
+                    ).asJsonString()))
+                }
+            }
+        }
 
         servers.add(server)
         return server
