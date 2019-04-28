596cc776799020bd7f123e2fb6f8803a547fbafe
DevNatan <itroken99@gmail.com>
2019-04-28 14:17:02 -0300

    Provide a method to make query for a server.

---
+++ backend/src/main/kotlin/me/devnatan/katan/backend/util/Minecraft.kt
@@ -0,0 +1,40 @@
+package me.devnatan.katan.backend.util
+
+import me.devnatan.katan.backend.server.KServerQuery
+import java.io.BufferedReader
+import java.io.DataOutputStream
+import java.io.InputStreamReader
+import java.net.InetSocketAddress
+import java.net.Socket
+import java.net.SocketException
+import kotlin.system.measureTimeMillis
+
+fun query(address: InetSocketAddress): KServerQuery {
+    val socket = Socket()
+    return try {
+        val latency = measureTimeMillis {
+            socket.connect(address, 3000)
+        }
+
+        val writter = DataOutputStream(socket.getOutputStream())
+        val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+
+        socket.use {
+            writter.write(byteArrayOf(0xFE.toByte(), 0x01.toByte()))
+            val line = reader.readLine() ?: return KServerQuery.offline()
+            val response = line.split("\u0000\u0000\u0000").map {
+                it.replace("\u0000", "")
+            }
+
+            KServerQuery(
+                response[2],
+                response[3],
+                response[4].toInt(),
+                response[5].toInt(),
+                latency
+            )
+        }
+    } catch (e: SocketException) {
+        KServerQuery.offline()
+    }
+}
