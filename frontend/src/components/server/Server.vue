27a7b04a421555267dfa8d1126d48a655080b471
DevNatan <itroken99@gmail.com>
2019-04-29 18:30:04 -0300

    Update the server according to the demand.

--- frontend/src/components/server/Server.vue
+++ frontend/src/components/server/Server.vue
@@ -7,22 +7,22 @@
         <p>
           <span><i data-feather="x"></i>{{ server.state === "STOPPED" ? "Offline" : "Online" }}</span>
           <span> • </span>
-          <span><i data-feather="play"></i>0/100</span>
+          <span><i data-feather="play"></i>{{ server.query.players + '/' + server.query.maxPlayers }}</span>
         </p>
       </header>
       <div class="m2">
-        <router-view/>
+        <router-view :server="server"/>
       </div>
     </div>
   </main>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
-import Header from "@/components/static/Header.vue";
-import Menu from "@/components/static/Menu.vue";
+  import {Component, Vue} from "vue-property-decorator";
+  import Header from "@/components/static/Header.vue";
+  import Menu from "@/components/static/Menu.vue";
 
-@Component({
+  @Component({
   components: {
     Header,
     Menu
@@ -30,16 +30,32 @@ import Menu from "@/components/static/Menu.vue";
 })
 export default class Server extends Vue {
   private serverId!: any;
-  private server: any;
-
   private isServerLoaded: boolean = false;
+
+  server?: any = null;
 
   created() {
     this.serverId = this.$route.params.serverId;
-    this.searchServer();
+    this.searchServer(() => {
+      const vm = Vue.prototype;
+      vm.$socket.on("message", (data: any) => {
+        if (data.reason == "server_updated") {
+          this.server = data.content.server;
+          return
+        }
+
+        switch (data.type) {
+          case "server-log": {
+            Vue.prototype.$bus.emit("server-log", data);
+            break;
+          }
+          default: console.error("Failed to handle server message: ", data)
+        }
+      })
+    });
   }
 
-  private searchServer() {
+  private searchServer(callback: () => void) {
       Vue.prototype.$http({
           method: "GET",
           url: "server/" + this.serverId
@@ -47,7 +63,7 @@ export default class Server extends Vue {
           if (response.status === 200) {
               this.server = response.data.message;
               this.isServerLoaded = true;
-              console.log(this.server);
+              callback();
           } else {
               // TODO: show error
           }
