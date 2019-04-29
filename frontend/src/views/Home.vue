cdeab872ed2b860174362f639d5eae836e7e9e2e
DevNatan <itroken99@gmail.com>
2019-04-29 20:11:17 -0300

    Remove header and menu from Home.

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,27 +1,12 @@
 <template>
   <main class="main">
-    <Menu />
-    <div class="inner">
-      <header class="header">
-        <h2>Heading</h2>
-      </header>
-      <div class="content">
-        <h4>Content</h4>
-      </div>
-    </div>
+    Home
   </main>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import Header from "@/components/static/Header.vue";
-import Menu from "@/components/static/Menu.vue";
 
-@Component({
-  components: {
-    Header,
-    Menu
-  }
-})
+@Component
 export default class Home extends Vue {}
 </script>
