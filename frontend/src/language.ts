95b43535ff3b815402a5b3435e3a0f4f01be531b
DevNatan <itroken99@gmail.com>
2019-05-01 12:06:47 -0300

    Possibility to use custom language.

---
+++ frontend/src/language.ts
@@ -0,0 +1,28 @@
+import Vue from "vue";
+
+export default class Language {
+
+    private values!: any;
+
+    constructor(callback: (result: any) => void) {
+        this.init((result) => {
+
+            callback(result);
+        });
+    }
+
+    get(key: string): any {
+        return this.values[key];
+    }
+
+    init(callback: (result: any) => void) {
+        Vue.prototype.$http({
+            method: "GET",
+            url: "/locale"
+        }).then((result: any) => {
+            this.values = result.data.message;
+            callback(result);
+        });
+    }
+
+}
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,17 +4,24 @@ import router from "./router";
 import Axios from "axios";
 import Socket from "@/socket";
 import Bus from "@/bus";
+import Language from "@/language";
 
 Vue.config.productionTip = false;
 
 const vm = Vue.prototype;
 
+Vue.filter('locale', function (key: string) {
+  let value = vm.$lang.get(key);
+  return value == null ? "[unknown key]" : value;
+});
+
 vm.$http = Axios.create({ baseURL: "http://localhost:8081/" });
 vm.$bus = new Bus();
 vm.$socket = new Socket("ws://localhost:8081/");
 vm.$socket.connect();
-
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount("#app");
+vm.$lang = new Language(() => {
+  new Vue({
+    router,
+    render: h => h(App)
+  }).$mount("#app");
+});
