ba012e80cc51c525b775a2a92e55155ba8351183
DevNatan <itroken99@gmail.com>
2019-05-01 12:08:26 -0300

    Add transition when change router view.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view />
+    <transition name="fade" mode="out-in">
+      <router-view />
+    </transition>
   </div>
 </template>
 <style src="@/assets/scss/main.scss" lang="scss"></style>
