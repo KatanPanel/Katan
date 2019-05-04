78e68f4ae292103709b4d950f292bcbf3a0171ab
DevNatan <itroken99@gmail.com>
2019-05-04 14:06:56 -0300

    Load servers from database instead of from servers folder.

--- backend/src/main/kotlin/me/devnatan/katan/backend/server/KServerManager.kt
+++ backend/src/main/kotlin/me/devnatan/katan/backend/server/KServerManager.kt
@@ -1,11 +1,15 @@
 package me.devnatan.katan.backend.server
 
 import io.netty.util.internal.ConcurrentSet
+import kotlinx.atomicfu.AtomicInt
+import kotlinx.atomicfu.atomic
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.sendBlocking
 import me.devnatan.katan.backend.Katan
 import me.devnatan.katan.backend.io.createProcess
+import me.devnatan.katan.backend.sql.Server
 import me.devnatan.katan.backend.util.asJsonString
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.slf4j.LoggerFactory
 import java.io.File
 import java.io.FileNotFoundException
@@ -18,58 +22,94 @@ class KServerManager(private val katan: Katan) {
 
     }
 
+    private val serversFolder: File by lazy {
+        val serversFolder = File(this::class.java.classLoader.getResource("servers").file)
+        if (!serversFolder.exists())
+            serversFolder.mkdir()
+        serversFolder
+    }
+    private val idCounter: AtomicInt = atomic(0)
     val servers: ConcurrentSet<KServer> = ConcurrentSet()
 
-    fun getServer(id: String): KServer? = servers.find { it.id.equals(id, true) }
+    fun getServer(id: Int): KServer? = servers.find { it.id == id }
+    fun getServer(name: String): KServer? = servers.find { it.name.equals(name, true) }
 
     fun load(coroutine: CoroutineScope) {
-        val folder = File(this::class.java.classLoader.getResource("servers").file)
-        if (!folder.exists())
-            folder.mkdir()
-
-        val folders = folder.listFiles() ?: return
-        for (serverPath in folders) {
-            if (!serverPath.isDirectory) continue
-
-            val serverId = serverPath.name
-            if (serverId.startsWith("-")) {
-                LOGGER.info("Ignoring server \"$serverId\".")
-                continue
+        transaction {
+            Server.all().forEach {
+                servers.add(loadServer(coroutine, it))
             }
-
-            if (!serverPath.canRead()) {
-                LOGGER.error("Access denied to read files in \"$serverId\".")
-                continue
-            }
-
-            loadServer(coroutine, serverId, serverPath)
         }
+
+        idCounter.value = servers.size
         LOGGER.info("Loaded ${servers.size} servers.")
     }
 
-    fun loadServer(coroutine: CoroutineScope, id: String, path: File): KServer {
-        val jar = path.listFiles().firstOrNull {
-            it.name.endsWith(".jar")
-        } ?: throw FileNotFoundException("Couldn't find PaperSpigot for server \"$id\".")
-
-        val server = KServer(
-            id,
-            KServerPath(path.absolutePath, jar.name),
-            createProcess(coroutine, path, "java", "-Xms256M", "-Xmx512M", "-jar", jar.name, "-o", "FALSE"),
-            EnumKServerState.STOPPED,
-            KServerQuery.offline()
-        ).apply {
-            onMessage = { message ->
-                katan.actor.sendBlocking(mapOf(
-                    "type" to "server-log",
-                    "server" to id,
-                    "message" to message
-                ).asJsonString())
-            }
+    fun loadServer(coroutine: CoroutineScope, server: Server): KServer? {
+        val folder = File(serversFolder, "server${server.serverId}")
+        if (folder.name.startsWith("-")) {
+            LOGGER.info("Ignoring server \"${server.name}\".")
+            return null
         }
 
-        servers.add(server)
-        return server
+        if (!folder.exists()) {
+            LOGGER.error("Access denied to read files in \"${server.name}\".")
+            return null
+        }
+
+        if (!folder.canRead()) {
+            LOGGER.error("Access denied to read files in \"${server.name}\".")
+            return null
+        }
+
+        val kserver = KServer(server.serverId, server.name,
+            KServerPath(server.pathRoot, server.jarFile),
+            createProcess(coroutine, folder, server.initParams),
+            EnumKServerState.STOPPED,
+            KServerQuery.offline(),
+            server.initParams
+        ).apply {
+            onMessage = { message ->
+                katan.actor.sendBlocking(
+                    mapOf(
+                        "type" to "server-log",
+                        "server" to id,
+                        "message" to message
+                    ).asJsonString()!!
+                )
+            }
+        }
+        servers.add(kserver)
+        return kserver
     }
 
+    fun createServer(coroutine: CoroutineScope, name: String, address: String, port: Int, memory: Int): Int {
+        val id = idCounter.incrementAndGet()
+        val file = copyDefaultServerFolder(serversFolder, "server$id")
+        val server = transaction {
+            Server.new {
+                this.serverId = id
+                this.name = name
+                this.address = address
+                this.port = port
+                this.pathRoot = file.absolutePath
+                this.jarFile = file.listFiles().firstOrNull {
+                    it.name.endsWith(".jar")
+                }!!.name
+                this.initParams = "java -Xms1024M -Xmx${memory}M -jar ${this.jarFile} -o FALSE PAUSE"
+            }
+        }
+        loadServer(coroutine, server)
+        return id
+    }
+
+    private fun copyDefaultServerFolder(path: File, to: String): File {
+        val default = File(path, "-default")
+        if (!default.exists())
+            throw FileNotFoundException("Server default folder doesn't exists")
+
+        val file = File(path, to)
+        default.copyRecursively(file)
+        return file
+    }
 }
