315883a01c0224174068fea8df7601b8b93581d9
DevNatan <itroken99@gmail.com>
2019-05-04 14:19:27 -0300

    Add icons and hidden text's to Menu.

--- frontend/src/components/static/Menu.vue
+++ frontend/src/components/static/Menu.vue
@@ -2,21 +2,70 @@
   <div class="menu">
     <div class="klogo">K</div>
     <ul>
+      <router-link tag="li" :to="{ name: 'server' }">
+        <a href="#">
+          <GridIcon />
+          <span>Home</span>
+        </a>
+      </router-link>
+      <router-link tag="li" :to="{ name: 'console' }">
+        <a href="#">
+          <TerminalIcon />
+          <span>Console</span>
+        </a>
+      </router-link>
       <li>
-        <a href="#"><i data-feather="home"></i></a>
+        <a href="#">
+          <FolderIcon />
+          <span>FTP Access</span>
+        </a>
       </li>
       <li>
-        <a href="#"><i data-feather="layers"></i></a>
+        <a href="#">
+          <SettingsIcon />
+          <span>Settings</span>
+        </a>
       </li>
       <li>
-        <a href="#"><i data-feather="smile"></i></a>
+        <a href="#">
+          <DownloadCloudIcon />
+          <span>Backup</span>
+        </a>
       </li>
       <li>
-        <a href="#"><i data-feather="file"></i></a>
-      </li>
-      <li>
-        <a href="#"><i data-feather="settings"></i></a>
+        <a href="#">
+          <HelpCircleIcon />
+          <span>Help</span>
+        </a>
       </li>
     </ul>
   </div>
 </template>
+<script lang="ts">
+// @ts-ignore
+import {
+  HelpCircleIcon,
+  GridIcon,
+  TerminalIcon,
+  FolderIcon,
+  HomeIcon,
+  SettingsIcon,
+  LogOutIcon,
+  DownloadCloudIcon
+} from "vue-feather-icons/icons";
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {
+    HelpCircleIcon,
+    GridIcon,
+    DownloadCloudIcon,
+    FolderIcon,
+    LogOutIcon,
+    SettingsIcon,
+    TerminalIcon,
+    HomeIcon
+  }
+})
+export default class Menu extends Vue {}
+</script>
