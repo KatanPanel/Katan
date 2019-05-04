2b434ba262f5ccda39b3abb62272e63236edad3a
DevNatan <itroken99@gmail.com>
2019-05-04 14:06:18 -0300

    Add MySQL support and configuration file.

--- backend/src/main/kotlin/me/devnatan/katan/backend/Katan.kt
+++ backend/src/main/kotlin/me/devnatan/katan/backend/Katan.kt
@@ -1,11 +1,10 @@
 package me.devnatan.katan.backend
 
-import com.google.gson.Gson
+import com.fasterxml.jackson.databind.ObjectMapper
 import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.actor
 import kotlinx.coroutines.channels.consumeEach
-import kotlinx.coroutines.runBlocking
 import me.devnatan.katan.backend.internal.SocketServer
 import me.devnatan.katan.backend.io.readFile
 import me.devnatan.katan.backend.message.Messenger
@@ -14,31 +13,50 @@ import me.devnatan.katan.backend.message.handler.ServerHandlerPredicate
 import me.devnatan.katan.backend.message.handler.StartServerHandler
 import me.devnatan.katan.backend.message.handler.StopServerHandler
 import me.devnatan.katan.backend.server.KServerManager
+import me.devnatan.katan.backend.sql.Servers
+import me.devnatan.katan.backend.util.asJsonMap
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.addLogger
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.io.File
 import java.io.FileNotFoundException
 import java.nio.charset.StandardCharsets
 
 class Katan {
 
-    private val coroutine: CoroutineScope = CoroutineScope(CoroutineName("Katan"))
+    companion object {
+
+        val LOGGER: Logger = LoggerFactory.getLogger("Katan")!!
+
+    }
+
+    val coroutine: CoroutineScope = CoroutineScope(CoroutineName("Katan"))
     val actor = CoroutineScope(CoroutineName("Katan:Logging")).actor<String> {
         consumeEach {
             socketServer.broadcast(it)
         }
     }
 
-    lateinit var gson: Gson
+    lateinit var json: ObjectMapper
     lateinit var locale: Map<*, *>
+    lateinit var config: Map<*, *>
 
     lateinit var socketServer: SocketServer
     lateinit var serverManager: KServerManager
     lateinit var messenger: Messenger
 
     internal fun init() {
-        runBlocking {
-            loadLocale()
-        }
+        locale = loadJsonResource("locale.json")
+        config = loadJsonResource("katan.json")
+        setupDatabase()
+        load()
+    }
 
+    private fun load() {
         socketServer = SocketServer()
         messenger = Messenger().apply {
             addPredicate(
@@ -53,14 +71,26 @@ class Katan {
         serverManager.load(coroutine)
     }
 
-    private suspend fun loadLocale() {
-        val f = File(this::class.java.classLoader.getResource("locale.json").file)
-        if (!f.exists())
-            throw FileNotFoundException("Couldn't find locale file")
+    private fun setupDatabase() {
+        val mysql = config["mysql"] as Map<*, *>
+        Database.connect(mysql["url"] as String,
+            mysql["driver"] as String,
+            mysql["user"] as String,
+            mysql["password"] as String)
 
-        readFile(coroutine, f) {
-            this.locale = gson.fromJson(String(it, StandardCharsets.UTF_8), Map::class.java)
+        transaction {
+            addLogger(StdOutSqlLogger)
+            SchemaUtils.create(Servers)
         }
+    }
+
+    private fun loadJsonResource(resource: String): Map<*, *> {
+        val f = File(this::class.java.classLoader.getResource(resource).file)
+        if (!f.exists())
+            throw FileNotFoundException("Couldn't find resource $resource")
+
+        return String(readFile(f), StandardCharsets.UTF_8).asJsonMap()
+            ?: throw IllegalArgumentException("Couldn't load resource $resource")
     }
 
 }
---
+++ backend/src/main/kotlin/me/devnatan/katan/backend/sql/Servers.kt
@@ -0,0 +1,31 @@
+package me.devnatan.katan.backend.sql
+
+import org.jetbrains.exposed.dao.EntityID
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.IntIdTable
+
+object Servers : IntIdTable("katan_servers") {
+
+    val serverId = integer("serverId").primaryKey()
+    val name = varchar("name", 50).uniqueIndex()
+    val address = varchar("address", 50)
+    val port = integer("port")
+    val pathRoot = varchar("path_root", 255)
+    val jarFile = varchar("jar_file", 255)
+    val initParams = varchar("init_params", 255)
+
+}
+
+class Server(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<Server>(Servers)
+
+    var serverId    by Servers.serverId
+    var name        by Servers.name
+    var address     by Servers.address
+    var port        by Servers.port
+    var pathRoot    by Servers.pathRoot
+    var jarFile     by Servers.jarFile
+    var initParams  by Servers.initParams
+
+}
