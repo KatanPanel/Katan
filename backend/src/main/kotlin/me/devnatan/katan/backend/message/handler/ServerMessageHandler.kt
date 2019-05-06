6b416195e31518c8f475adbf21ab400ad25af108
DevNatan <itroken99@gmail.com>
2019-05-06 11:00:42 -0300

    Remove things from Gson and use API interfaces.

--- backend/src/main/kotlin/me/devnatan/katan/backend/message/handler/ServerMessageHandler.kt
+++ backend/src/main/kotlin/me/devnatan/katan/backend/message/handler/ServerMessageHandler.kt
@@ -1,9 +1,9 @@
 @file:JvmMultifileClass
 package me.devnatan.katan.backend.message.handler
 
+import me.devnatan.katan.api.server.ServerState
 import me.devnatan.katan.backend.katan
 import me.devnatan.katan.backend.message.*
-import me.devnatan.katan.backend.server.EnumKServerState
 
 object ServerHandlerPredicate : MessageHandlerPredicate {
 
@@ -18,17 +18,17 @@ object StartServerHandler : MessageHandler {
     override suspend fun handle(message: IncomingMessage) {
         if (!message.isCommand("start-server")) return
 
-        katan.serverManager.getServer((message["server"] as Double).toInt())?.let {
-            if (it.state == EnumKServerState.STOPPED)
-                it.startAsync {
-                    katan.socketServer.broadcast(MessageImpl(
+        katan.serverController.getServer(message["server"] as Int)?.let {
+            if (it.state == ServerState.STOPPED)
+                it.start {
+                    katan.socketController.broadcast(MessageImpl(
                         MessageReason.SERVER_UPDATED,
                         MessageType.COMMAND,
                         mapOf("server" to it)
                     ))
                 }
             else
-                katan.socketServer.broadcast(MessageImpl(
+                katan.socketController.broadcast(MessageImpl(
                     MessageReason.SERVER_ALREADY_STARTED,
                     MessageType.MESSAGE,
                     mapOf("server" to it.id)
@@ -43,10 +43,10 @@ object StopServerHandler : MessageHandler {
     override suspend fun handle(message: IncomingMessage) {
         if (!message.isCommand("stop-server")) return
 
-        katan.serverManager.getServer((message["server"] as Double).toInt())?.let {
-            if (it.state != EnumKServerState.STOPPED) {
+        katan.serverController.getServer(message["server"] as Int)?.let {
+            if (it.state != ServerState.STOPPED) {
                 it.stop {
-                    katan.socketServer.broadcast(MessageImpl(
+                    katan.socketController.broadcast(MessageImpl(
                         MessageReason.SERVER_UPDATED,
                         MessageType.COMMAND,
                         mapOf("server" to it)
@@ -63,9 +63,9 @@ object InputServerHandler : MessageHandler {
     override suspend fun handle(message: IncomingMessage) {
         if (!message.isCommand("input-server")) return
 
-        katan.serverManager.getServer((message["server"] as Double).toInt())?.let {
-            if (it.state == EnumKServerState.RUNNING) {
-                it.write(message["input"] as String)
+        katan.serverController.getServer(message["server"] as Int)?.let {
+            if (it.state == ServerState.RUNNING) {
+                it.process.write(message["input"] as String)
             }
         }
     }
