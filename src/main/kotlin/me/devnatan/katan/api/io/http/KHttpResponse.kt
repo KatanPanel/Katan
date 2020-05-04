7fc59b0761b94dc40a1e8678b5d60fd244be0d55
DevNatan <itroken99@gmail.com>
2020-05-04 18:48:56 -0300

    Message content cannot be private

--- src/main/kotlin/me/devnatan/katan/api/io/http/KHttpResponse.kt
+++ src/main/kotlin/me/devnatan/katan/api/io/http/KHttpResponse.kt
@@ -1,23 +1,23 @@
 package me.devnatan.katan.api.io.http
 
 /**
- * @property response the server's response, which can be "success" or "error".
+ * @property response the server's response, which can be "success" or "error"
  */
 sealed class KHttpResponse(
     private val response: String
 ) {
 
     /**
-     * @param TData return type of message content
-     * @property data content of the response to the request.
+     * @param T return type of message content
+     * @property data content of the response to the request
      */
-    class Ok<out TData : Any>(
-        private val data: TData
+    class Ok<out T : Any>(
+        val data: T
     ) : KHttpResponse("success")
 
     /**
-     * @property code error code to better identify it.
-     * @property message any message that the response may have, usually used for error messages.
+     * @property code error code to better identify it
+     * @property message any message that the response may have, usually used for error messages
      */
     class Error(
         val code: Number,
