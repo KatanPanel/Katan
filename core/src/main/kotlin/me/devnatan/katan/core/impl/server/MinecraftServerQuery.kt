1abc06c31f34626354b951a5429556f4cd86ec1a
DevNatan <itroken99@gmail.com>
2020-09-14 01:06:40 -0300

    Minecraft server query members.

--- core/src/main/kotlin/me/devnatan/katan/core/impl/server/MinecraftServerQuery.kt
+++ core/src/main/kotlin/me/devnatan/katan/core/impl/server/MinecraftServerQuery.kt
@@ -1,4 +1,13 @@
 package me.devnatan.katan.core.impl.server
 
-class MinecraftServerQuery {
+import me.devnatan.katan.api.server.Server
+import me.devnatan.katan.api.server.ServerQuery
+import java.time.Instant
+
+class MinecraftServerQuery(val server: Server) : ServerQuery {
+
+    override var latency: Long = 0
+
+    override var lastQueriedAt: Instant? = null
+
 }
