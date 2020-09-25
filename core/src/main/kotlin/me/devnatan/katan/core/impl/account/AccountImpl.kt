f4dd1b47d9360908c46029c9d7cd4a742724f62a
DevNatan <itroken99@gmail.com>
2020-09-25 16:55:10 -0300

    fix: since moving to kotlinx.serialization we need it

--- core/src/main/kotlin/me/devnatan/katan/core/impl/account/AccountImpl.kt
+++ core/src/main/kotlin/me/devnatan/katan/core/impl/account/AccountImpl.kt
@@ -1,17 +1,24 @@
 package me.devnatan.katan.core.impl.account
 
 import com.google.common.collect.Maps
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 import me.devnatan.katan.api.account.Account
 import me.devnatan.katan.api.permission.Permission
 import me.devnatan.katan.api.permission.PermissionFlag
 import java.util.*
 
+@Serializable
 data class AccountImpl(
+    @Contextual
     override val id: UUID,
     override val username: String,
-    @Transient override var password: String
+    @Transient
+    override var password: String = ""
 ) : Account {
 
+    @Contextual
     override var permissions: EnumMap<Permission, PermissionFlag> = Maps.newEnumMap(Permission::class.java)
 
 }
--- core/src/main/kotlin/me/devnatan/katan/core/impl/server/ServerImpl.kt
+++ core/src/main/kotlin/me/devnatan/katan/core/impl/server/ServerImpl.kt
@@ -1,12 +1,17 @@
 package me.devnatan.katan.core.impl.server
 
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 import me.devnatan.katan.api.server.*
 
+@Serializable
 class ServerImpl(
     override val id: Int,
     override var name: String,
     override var address: String,
     override var port: Int,
+    @Contextual
     override val container: ServerContainer
 ) : Server {
 
