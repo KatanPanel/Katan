a724cb14fbdb8025a1090e60683d450483c5adb0
DevNatan <itroken99@gmail.com>
2020-09-28 01:56:51 -0300

    refactor(ws): better CSRF and CORS logging

--- web-server/src/main/kotlin/me/devnatan/katan/webserver/Setup.kt
+++ web-server/src/main/kotlin/me/devnatan/katan/webserver/Setup.kt
@@ -1,6 +1,10 @@
 package me.devnatan.katan.webserver
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.auth.jwt.*
 import io.ktor.features.*
 import io.ktor.http.*
 import io.ktor.locations.*
@@ -19,7 +23,7 @@ internal fun Application.installHooks() {
     install(WebSockets)
     install(Locations)
     install(HSTS) {
-        logger.info("Enabled Strict Transport Security`")
+        logger.info("Enabled Strict Transport Security")
     }
 
     install(ContentNegotiation) {
@@ -29,27 +33,6 @@ internal fun Application.installHooks() {
     }
 
     install(CallLogging)
-
-    install(CORS) {
-        logger.info("Installing CORS feature...")
-        allowCredentials = true
-
-        val cors = kws.config.getConfig("security.cors")
-        if (cors.get("allowAnyHost", false)) {
-            logger.info("All hosts have been allowed through CORS.")
-            anyHost()
-        } else if (cors.hasPath("hosts")) {
-            for (hostConfig in cors.getConfigList("hosts")) {
-                host(
-                    hostConfig.getString("hostname"),
-                    hostConfig.get("schemes", emptyList()),
-                    hostConfig.get("subDomains", emptyList())
-                )
-            }
-            hosts.forEach { logger.info("Allowed $it host through CORS.") }
-        }
-    }
-
     install(StatusPages) {
         exception<Throwable> { cause ->
             call.response.status(HttpStatusCode.InternalServerError)
@@ -57,14 +40,68 @@ internal fun Application.installHooks() {
         }
     }
 
+    install(CORS) {
+        allowCredentials = true
+
+        val cors = kws.config.getConfig("cors")
+        if (cors.get("allowAnyHost", false)) {
+            logger.info("All hosts have been allowed through CORS.")
+            anyHost()
+        } else if (cors.hasPath("hosts")) {
+            logger.info(
+                "The following hosts ${
+                    cors.getConfigList("hosts").map { config ->
+                        Triple(
+                            config.getString("hostname"),
+                            config.get("schemes", emptyList<String>()),
+                            config.get("subDomains", emptyList<String>())
+                        )
+                    }.onEach { (hostname, schemes, subdomains) ->
+                        host(hostname, schemes, subdomains)
+                    }.joinToString(", ") { (hostname, schemes, subDomains) ->
+                        buildString {
+                            append(schemes.joinToString(", ", prefix = "(", postfix = ")"))
+                            append("://")
+
+                            if (subDomains.isNotEmpty()) {
+                                append(subDomains.joinToString(", ", prefix = "[", postfix = "]"))
+                                append(".")
+                            }
+                            
+                            append(hostname)
+                        }
+                    }
+                } have been allowed through in CORS. "
+            )
+        }
+    }
+
     install(CsrfProtection) {
-        logger.info("Installing CSRF feature...")
-        for (whitelist in kws.config.getConfigList("security.csrf.whitelist")) {
-            val protocol = whitelist.getString("protocol")
-            val hostname = whitelist.getString("hostname")
-            val port = whitelist.getInt("port")
+        logger.info("Whitelisted CSRF hosts: " + kws.config.getConfigList("csrf.hosts").map { host ->
+            Triple(host.getString("protocol"), host.getString("hostname"), host.getInt("port"))
+        }.onEach { (protocol, hostname, port) ->
             validate(OriginMatchesKnownHost(protocol, hostname, port))
-            logger.info("[CSRF] Allowed $protocol://$hostname:$port.")
+        }.joinToString(", ") { (protocol, hostname, port) ->
+            "$protocol://$hostname:$port"
+        })
+    }
+
+    install(Authentication) {
+        val config = kws.config.getConfig("jwt")
+        val audience = config.getString("audience")
+        jwt {
+            realm = "Katan WebServer"
+            verifier(
+                JWT.require(Algorithm.HMAC256(config.getString("secret")))
+                    .withAudience(audience)
+                    .withIssuer(config.getString("issuer"))
+                    .build()
+            )
+            validate { credential ->
+                if (credential.payload.audience.contains(audience))
+                    JWTPrincipal(credential.payload)
+                else null
+            }
         }
     }
 
