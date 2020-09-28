4381e55aab38a88fff9af55ebc541e8a4d5de55a
DevNatan <itroken99@gmail.com>
2020-09-28 19:54:54 -0300

    fix(ws): HTTP Error 404 when accessing routes with CSRF protection

--- web-server/src/main/kotlin/me/devnatan/katan/webserver/routes/Auth.kt
+++ web-server/src/main/kotlin/me/devnatan/katan/webserver/routes/Auth.kt
@@ -4,11 +4,27 @@ package me.devnatan.katan.webserver.routes
 
 import io.ktor.locations.*
 
-@Location("/auth/login")
-data class LoginRoute(val username: String, val password: String)
+@Location("/auth")
+class AuthRoute {
 
-@Location("/auth/register")
-data class RegisterRoute(val username: String, val password: String)
+    @Location("/login")
+    data class LoginRoute(
+        val username: String = "",
+        val password: String = "",
+        val parent: AuthRoute
+    )
 
-@Location("/auth/verify")
-data class VerifyRoute(val token: String)
+    @Location("/register")
+    data class RegisterRoute(
+        val username: String = "",
+        val password: String = "",
+        val parent: AuthRoute
+    )
+
+    @Location("/verify")
+    data class VerifyRoute(
+        val token: String = "",
+        val parent: AuthRoute
+    )
+
+}
