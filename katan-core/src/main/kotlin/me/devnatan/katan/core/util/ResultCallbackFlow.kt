c918e00dbfc66c73fba38dbbff9c53d17a8d2b90
DevNatan <itroken99@gmail.com>
2021-02-07 14:02:12 -0300

    fix(core): propagate error during callback execution

--- katan-core/src/main/kotlin/me/devnatan/katan/core/util/ResultCallbackFlow.kt
+++ katan-core/src/main/kotlin/me/devnatan/katan/core/util/ResultCallbackFlow.kt
@@ -9,30 +9,25 @@ import kotlinx.coroutines.channels.sendBlocking
 import java.io.Closeable
 import kotlin.coroutines.cancellation.CancellationException
 
-@OptIn(ExperimentalCoroutinesApi::class)
-class FlowResultCallback<T, R>(private val scope: ProducerScope<R>, inline val transform: (T) -> R) : ResultCallback<T> {
-
-    private var lastValue: String? = null
+abstract class AbstractResultCallback<T> : ResultCallback<T> {
 
     private var stream: Closeable? = null
-    private var closed = false
 
-    override fun onStart(closeable: Closeable) {
-        if (closed) return
+    override fun onStart(closeable: Closeable?) {
+        stream?.close()
         stream = closeable
-    }
-
-    override fun onNext(value: T) {
-        if (closed || stream == null) return
-        try {
-            scope.sendBlocking(transform(value))
-        } catch (e: Throwable) {
-            close0()
-        }
     }
 
     override fun onError(cause: Throwable) {
         close0(cause)
+    }
+
+    override fun onNext(value: T) {
+        try {
+            handle(value)
+        } catch (e: Throwable) {
+            close0(e)
+        }
     }
 
     override fun onComplete() {
@@ -44,12 +39,35 @@ class FlowResultCallback<T, R>(private val scope: ProducerScope<R>, inline val t
     }
 
     private fun close0(cause: Throwable? = null) {
-        if (closed) return
+        if (isClosed()) return
+        finalize(cause)
         stream?.close()
-        scope.cancel(cause?.let { if (it !is CancellationException) CancellationException(it) else it })
         stream = null
-        lastValue = null
-        closed = true
+    }
+
+    private fun isClosed(): Boolean {
+        return stream == null
+    }
+
+    abstract fun handle(value: T)
+
+    abstract fun finalize(error: Throwable?)
+
+}
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class FlowResultCallback<T, R>(private val scope: ProducerScope<R>, inline val transform: (T) -> R) : AbstractResultCallback<T>() {
+
+    override fun handle(value: T) {
+        scope.sendBlocking(transform(value))
+    }
+
+    override fun finalize(error: Throwable?) {
+        scope.cancel(error?.let { cause ->
+            if (cause !is CancellationException)
+                CancellationException(cause)
+            else cause
+        })
     }
 
 }
@@ -57,45 +75,14 @@ class FlowResultCallback<T, R>(private val scope: ProducerScope<R>, inline val t
 @OptIn(ExperimentalCoroutinesApi::class)
 class DeferredResultCallback<T, R>(
     inline val transform: (T) -> R
-) : ResultCallback<T>, CompletableDeferred<R> by CompletableDeferred() {
+) : AbstractResultCallback<T>(), CompletableDeferred<R> by CompletableDeferred() {
 
-    private var stream: Closeable? = null
-    private var closed = false
-
-    override fun onStart(closeable: Closeable) {
-        if (closed) return
-        stream = closeable
+    override fun handle(value: T) {
+        complete(transform(value))
     }
 
-    override fun onNext(value: T) {
-        if (closed) return
-        try {
-            complete(transform(value))
-        } catch (e: Throwable) {
-            close0()
-        }
-    }
-
-    override fun onError(cause: Throwable) {
-        close0(cause)
-    }
-
-    override fun onComplete() {
-        close0()
-    }
-
-    override fun close() {
-        close0()
-    }
-
-    private fun close0(cause: Throwable? = null) {
-        if (closed) return
-        cause?.let {
-            completeExceptionally(it)
-        }
-        stream?.close()
-        stream = null
-        closed = true
+    override fun finalize(error: Throwable?) {
+        error?.let { cause -> completeExceptionally(cause) }
     }
 
 }
