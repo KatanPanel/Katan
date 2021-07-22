18f5ed6bff085338f7c511be07cd657cec3c0ed0
Natan Vieira <itroken99@gmail.com>
2021-07-22 17:45:32 -0300

    chore: apply javadoc and maven publication to all modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version Dependencies.kotlinVersion
+    id("org.jetbrains.dokka") version "1.4.10.2"
+    `maven-publish`
 }
 
 val projectGroup = "me.devnatan.katan"
@@ -10,11 +12,19 @@ val projectVersion = "0.0.1"
 group = projectGroup
 version = projectVersion
 
-allprojects {
+buildscript {
+    dependencies {
+
+    }
+}
+
+subprojects {
     group = projectGroup
     version = projectVersion
 
     apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.dokka")
+    apply<MavenPublishPlugin>()
 
     repositories {
         mavenCentral()
@@ -42,4 +52,34 @@ allprojects {
             }
         }
     }
+
+    java {
+        withJavadocJar()
+        withSourcesJar()
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                from(components["java"])
+
+                pom {
+                    scm {
+                        url.set("https://github.com/KatanPanel/Katan/tree/master/$artifactId")
+                    }
+
+                    licenses {
+                        license {
+                            name.set("The MIT License (MIT)")
+                            url.set("https://github.com/KatanPanel/Katan/blob/master/LICENSE")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+tasks.dokkaHtmlMultiModule.configure {
+    outputDirectory.set(buildDir.resolve("dokkaCustomMultiModuleOutput"))
 }
--- katan-api/build.gradle.kts
+++ katan-api/build.gradle.kts
@@ -14,41 +14,9 @@
  * limitations under the License.
  */
 
-plugins {
-    `maven-publish`
-    id("org.jetbrains.dokka") version "1.4.10.2"
-}
-
 dependencies {
     api("com.typesafe:config:1.4.0")
     api("br.com.devsrsouza.eventkt:eventkt-core-jvm:0.1.0-SNAPSHOT")
     api("org.slf4j:slf4j-api:1.7.30")
     api(Dependencies.config4k)
-}
-
-publishing {
-    publications {
-        val sources by tasks.registering(Jar::class) {
-            archiveClassifier.set("sources")
-            from(sourceSets.main.get().allSource)
-        }
-
-        create<MavenPublication>("maven") {
-            from(components["kotlin"])
-            artifact(sources.get())
-
-            pom {
-                scm {
-                    url.set("https://github.com/KatanPanel/Katan/tree/master/api")
-                }
-
-                licenses {
-                    license {
-                        name.set("The MIT License (MIT)")
-                        url.set("https://github.com/KatanPanel/Katan/blob/master/LICENSE")
-                    }
-                }
-            }
-        }
-    }
 }
