76d279a8c84621f8b9a3fbf008e6f5b00617954a
Natan Vieira do Nascimento <natanvnascimento@gmail.com>
2022-02-20 19:25:48 -0300

    Delete katan-cli module

--- katan-cli/src/main/kotlin/me/devnatan/katan/cli/KatanCLIException.kt
+++
@@ -1,7 +0,0 @@
-package me.devnatan.katan.cli
-
-class KatanCLIException(
-    translationKey: String,
-    cause: Throwable? = null,
-    vararg val translationArgs: Any
-) : Exception(translationKey, cause)
