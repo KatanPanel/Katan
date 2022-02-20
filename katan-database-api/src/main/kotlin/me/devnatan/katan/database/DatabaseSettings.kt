7b5ece1e21f73eeb876dcb78f39afcd8ca518f71
Natan Vieira do Nascimento <natanvnascimento@gmail.com>
2022-02-20 19:29:32 -0300

    Delete katan-database-api and katan-database-jdbc modules

--- katan-database-api/src/main/kotlin/me/devnatan/katan/database/DatabaseSettings.kt
+++
@@ -1,9 +0,0 @@
-package me.devnatan.katan.database
-
-interface DatabaseSettings {
-
-    operator fun <T> get(key: String): T? {
-        throw UnsupportedOperationException("Not implemented")
-    }
-
-}
