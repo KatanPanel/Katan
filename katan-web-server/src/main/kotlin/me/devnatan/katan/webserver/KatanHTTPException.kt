cb44f566b0a8e30a3353bea8534dbcee8d57ae47
Natan Vieira do Nascimento <natanvnascimento@gmail.com>
2022-03-11 22:17:10 -0300

    Delete `katan-web-server` module

--- katan-web-server/src/main/kotlin/me/devnatan/katan/webserver/KatanHTTPException.kt
+++
@@ -1,8 +0,0 @@
-package me.devnatan.katan.webserver
-
-import io.ktor.http.*
-
-class KatanHTTPException(
-    val errorCode: Int,
-    val status: HttpStatusCode,
-) : RuntimeException()
--- katan-web-server/src/main/kotlin/me/devnatan/katan/webserver/auth/jwt/AccountPrincipal.kt
+++
@@ -1,6 +0,0 @@
-package me.devnatan.katan.webserver.auth.jwt
-
-import io.ktor.auth.*
-import me.devnatan.katan.api.security.account.Account
-
-data class AccountPrincipal(val account: Account) : Principal
