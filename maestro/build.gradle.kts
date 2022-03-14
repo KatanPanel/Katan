6102f2051587e32ef772c27b1025374d64501dd8
Natan Vieira do Nascimento <natanvnascimento@gmail.com>
2022-03-14 20:28:53 -0300

    Add Yoki dependency

--- maestro/build.gradle.kts
+++ maestro/build.gradle.kts
@@ -1,3 +1,8 @@
+repositories {
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+}
+
 dependencies {
     implementation(project(":data-types"))
+    implementation(rootProject.libs.yoki)
 }
