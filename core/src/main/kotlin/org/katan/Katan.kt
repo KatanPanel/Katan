4ba33e0ff126134e6d02b672bbccf72bf2defad5
Natan Vieira <natanvnascimento@gmail.com>
2022-03-20 20:02:58 -0300

    Use `40055` as default port

--- core/src/main/kotlin/org/katan/Katan.kt
+++ core/src/main/kotlin/org/katan/Katan.kt
@@ -16,19 +16,29 @@ import java.io.Closeable
 class Katan : KoinComponent, Closeable {
 	
 	companion object {
+		private const val DEFAULT_PORT: Int = 40055
 		val LOGGER: Logger = LogManager.getLogger(Katan::class.java)
 	}
 	
 	private val maestro: Maestro = get()
 	val serverFactory: ServerFactory = get()
+	val port by lazy { selectPort() }
 	
 	fun start() {
-		LOGGER.info("Katan started")
+		LOGGER.info("Katan started on $port")
 	}
 	
 	override fun close() {
 		maestro.close()
 		LOGGER.info("Katan shutdown")
+	}
+
+	/**
+	 * Gets the port that serve will be run on from the environment variable
+	 * or use the [DEFAULT_PORT] if the port environment variable is not defined
+	 */
+	private fun selectPort(): Int {
+		return System.getenv("PORT").toIntOrNull() ?: DEFAULT_PORT
 	}
 	
 }
