e4c38eb20ce3717664c805b8b4f0b86966db4552
Natan Vieira <itroken99@gmail.com>
2022-05-28 13:31:24 -0300

    fix(app): register HttpModule on init

--- application/src/main/kotlin/org/katan/Application.kt
+++ application/src/main/kotlin/org/katan/Application.kt
@@ -2,6 +2,7 @@ package org.katan
 
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
+import org.katan.http.HttpModule
 import org.katan.runtime.di.RuntimeModule
 import org.katan.service.container.ContainerServiceModule
 import org.katan.service.server.di.ServerServiceModule
@@ -16,7 +17,7 @@ private object Application {
     fun main(args: Array<String>) {
         startKoin {
             logger(KoinLog4jLogger())
-            modules(RuntimeModule, ContainerServiceModule, ServerServiceModule)
+            modules(HttpModule, RuntimeModule, ContainerServiceModule, ServerServiceModule)
         }
 
         val app = Katan()
