ecf7bf6ea8643ddd62c37e5030065d32031846e7
Natan Vieira <itroken99@gmail.com>
2022-05-28 14:44:08 -0300

    fix(service/server): mark ServerImpl as serializable

--- services/service-server/src/main/kotlin/org/katan/service/server/ServerImpl.kt
+++ services/service-server/src/main/kotlin/org/katan/service/server/ServerImpl.kt
@@ -2,6 +2,7 @@ package org.katan.service.server
 
 import org.katan.service.container.Container
 
+@kotlinx.serialization.Serializable
 public data class ServerImpl(
     override val id: String,
     override val name: String,
