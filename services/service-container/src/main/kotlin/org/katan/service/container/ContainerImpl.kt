c58eb690d9502ef9efe1220a8e17798f0bb068eb
Natan Vieira <itroken99@gmail.com>
2022-05-28 14:43:47 -0300

    fix(service/container): mark ContainerImpl as serialization

--- services/service-container/src/main/kotlin/org/katan/service/container/ContainerImpl.kt
+++ services/service-container/src/main/kotlin/org/katan/service/container/ContainerImpl.kt
@@ -1,5 +1,6 @@
 package org.katan.service.container
 
+@kotlinx.serialization.Serializable
 internal data class ContainerImpl(
     override val id: String,
     override val runtimeIdentifier: String
