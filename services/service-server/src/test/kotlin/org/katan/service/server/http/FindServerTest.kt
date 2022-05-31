daf83b18f6e5fa5837339f3b58ce03f75670c884
Natan Vieira <itroken99@gmail.com>
2022-05-31 11:05:02 -0300

    refactor(service-server): rename test class names

--- services/service-server/src/test/kotlin/org/katan/service/server/http/FindServerTest.kt
+++ services/service-server/src/test/kotlin/org/katan/service/server/http/FindServerTest.kt
@@ -1,28 +1,30 @@
 package org.katan.service.server.http
 
-import io.ktor.client.plugins.resources.post
+import io.ktor.client.plugins.resources.get
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.routing.routing
 import org.katan.http.createTestClient
 import org.katan.http.withTestApplication
+import org.katan.service.server.ServerCreateOptions
+import org.katan.service.server.ServerService
 import org.katan.service.server.http.locations.Servers
 import org.katan.service.server.http.routes.createServer
+import org.katan.service.server.http.routes.findServer
 import org.koin.test.KoinTest
+import org.koin.test.get
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-class CreateEndpointTest : KoinTest {
+class FindServerTest : KoinTest {
 
     @Test
-    fun `should respond bad request on missing parameters`() = withTestApplication(
-        setup = {
-            routing {
-                createServer()
-            }
+    fun `when server is not found expect 400`() = withTestApplication({
+        routing {
+            findServer()
         }
-    ) {
+    }) {
         val testClient = createTestClient()
-        val request = testClient.post(Servers())
+        val request = testClient.get(Servers.Get(id = "unknown"))
 
         assertEquals(HttpStatusCode.BadRequest, request.status)
     }
