c1b719d02c2d62ff648dc3300318682c959529b7
Natan Vieira <itroken99@gmail.com>
2022-07-02 14:10:19 -0300

    Select HTTP server port properly

--- core/src/main/kotlin/org/katan/Katan.kt
+++ core/src/main/kotlin/org/katan/Katan.kt
@@ -6,14 +6,7 @@ import org.katan.http.server.HttpServer
 import org.koin.core.component.KoinComponent
 import java.io.Closeable
 
-/**
- * Katan's core class.
- *
- * @constructor Constructs a new Katan Core instance.
- */
-class Katan(
-    val httpServer: HttpServer = HttpServer(port = 8080)
-) : KoinComponent, Closeable {
+class Katan : KoinComponent {
 
     companion object {
         private const val DEFAULT_PORT: Int = 40055
@@ -21,19 +14,19 @@ class Katan(
     }
 
     private val port: Int by lazy { selectPort() }
+    private val httpServer: HttpServer = HttpServer(port = port)
 
     fun start() {
         httpServer.start()
-        LOGGER.info("Katan started on $port")
     }
 
-    override fun close() {
+    fun close() {
         httpServer.stop()
         LOGGER.info("Katan shutdown")
     }
 
     /**
-     * Gets the port that serve will be run on from the environment variable
+     * Selects the port that serve will be run on from the environment variable
      * or use the [DEFAULT_PORT] if the port environment variable is not defined.
      */
     private fun selectPort(): Int {
