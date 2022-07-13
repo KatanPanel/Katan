61c40504be3b503b266ac09b33f1784332c54fa9
Natan Vieira <itroken99@gmail.com>
2022-07-13 18:15:06 -0300

    Make UnitStatus serializable

--- model/src/main/kotlin/org/katan/model/unit/UnitStatus.kt
+++ model/src/main/kotlin/org/katan/model/unit/UnitStatus.kt
@@ -1,9 +1,11 @@
 package org.katan.model.unit
 
-interface UnitStatus {
+@kotlinx.serialization.Serializable
+sealed interface UnitStatus {
 
     val name: String
 
+    @kotlinx.serialization.Serializable
     object Unknown : UnitStatus {
         override val name: String = "unknown"
     }
