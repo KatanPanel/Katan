4c4027c8b4cad9cc08e21cde6c261e7852402f7d
Natan Vieira <itroken99@gmail.com>
2022-07-19 16:42:29 -0300

    Move UnitInstance impl to same package as interface

--- model/src/main/kotlin/org/katan/model/unit/UnitInstance.kt
+++ model/src/main/kotlin/org/katan/model/unit/UnitInstance.kt
@@ -1,9 +1,18 @@
 package org.katan.model.unit
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerialName
 import org.katan.model.Connection
 
 interface UnitInstance {
 
     val remoteAddress: Connection
 
+}
+
+@Serializable
+private data class UnitInstanceImpl(override val remoteAddress: Connection) : UnitInstance
+
+fun UnitInstance(remoteAddress: Connection): UnitInstance {
+    return UnitInstanceImpl(remoteAddress)
 }
