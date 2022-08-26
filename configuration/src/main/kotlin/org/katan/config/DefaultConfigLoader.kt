252e7883fb721a8c362fcea244ff3e3e516cead3
Natan Vieira <itroken99@gmail.com>
2022-08-26 00:22:39 -0300

    Example and prod application configuration

--- configuration/src/main/kotlin/org/katan/config/DefaultConfigLoader.kt
+++ configuration/src/main/kotlin/org/katan/config/DefaultConfigLoader.kt
@@ -1,16 +1,15 @@
 package org.katan.config
 
 import com.sksamuel.hoplite.ConfigLoaderBuilder
-import com.sksamuel.hoplite.addResourceSource
 
 internal object DefaultConfigLoader : ConfigLoader {
 
     override fun load(): KatanConfig {
         return ConfigLoaderBuilder.default()
-            .addResourceSource("/application-prod.conf", optional = true)
-            .addResourceSource("/application-dev.conf", optional = true)
             .strict()
             .build()
-            .loadConfigOrThrow<ConfigImpl>("/application.conf")
+            .loadConfigOrThrow<ConfigImpl>(
+                "/application.conf"
+            )
     }
 }
