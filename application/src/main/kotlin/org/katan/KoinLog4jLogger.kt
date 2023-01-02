60ab86fda65fbb0ed97f94703aa9303424cc1e4b
Natan Vieira <natanvnascimento@gmail.com>
2023-01-02 14:16:18 -0300

    Add typealias to Koin logger

--- application/src/main/kotlin/org/katan/KoinLog4jLogger.kt
+++ application/src/main/kotlin/org/katan/KoinLog4jLogger.kt
@@ -2,9 +2,9 @@ package org.katan
 
 import org.apache.logging.log4j.LogManager
 import org.koin.core.Koin
-import org.koin.core.logger.Level
-import org.koin.core.logger.Logger
 import org.koin.core.logger.MESSAGE
+import org.koin.core.logger.Level as KoinLogLevel
+import org.koin.core.logger.Logger as KoinLogger
 
 typealias Log4jLogger = org.apache.logging.log4j.Logger
 typealias Log4jLogLevel = org.apache.logging.log4j.Level
@@ -12,11 +12,13 @@ typealias Log4jLogLevel = org.apache.logging.log4j.Level
 /**
  * Implementation of Koin's Logger backed by a Log4j2 Logger
  */
-internal class KoinLog4jLogger : Logger(Level.DEBUG) {
+internal class KoinLog4jLogger : KoinLogger(KoinLogLevel.DEBUG) {
 	
-    private val backingLogger: Log4jLogger = LogManager.getLogger(Koin::class.java)
+    companion object {
+        private val backingLogger: Log4jLogger = LogManager.getLogger(Koin::class.java)
+    }
 	
-    override fun log(level: Level, msg: MESSAGE) {
+    override fun log(level: KoinLogLevel, msg: MESSAGE) {
         backingLogger.log(transformLevel(level), msg)
     }
 	
@@ -24,11 +26,11 @@ internal class KoinLog4jLogger : Logger(Level.DEBUG) {
      * Transform a Koin log level to Log4J log level.
      * @param level The Koin log level.
      */
-    private fun transformLevel(level: Level): Log4jLogLevel {
+    private fun transformLevel(level: KoinLogLevel): Log4jLogLevel {
         return when (level) {
-            Level.DEBUG -> Log4jLogLevel.DEBUG
-            Level.INFO -> Log4jLogLevel.INFO
-            Level.ERROR -> Log4jLogLevel.ERROR
+            KoinLogLevel.DEBUG -> Log4jLogLevel.DEBUG
+            KoinLogLevel.INFO -> Log4jLogLevel.INFO
+            KoinLogLevel.ERROR -> Log4jLogLevel.ERROR
             else -> Log4jLogLevel.ALL
         }
     }
