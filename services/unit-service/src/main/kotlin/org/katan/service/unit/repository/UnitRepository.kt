44079f2c173058d05fc4fef0044be57f29d8ca9d
Natan Vieira do Nascimento <itroken99@gmail.com>
2023-08-19 00:59:00 -0300

    Mark UnitRepository as internal

--- services/unit-service/src/main/kotlin/org/katan/service/unit/repository/UnitRepository.kt
+++ services/unit-service/src/main/kotlin/org/katan/service/unit/repository/UnitRepository.kt
@@ -4,20 +4,20 @@ import org.katan.model.unit.KUnit
 import org.katan.model.unit.auditlog.AuditLogEntry
 import org.katan.service.unit.model.UnitUpdateOptions
 
-public interface UnitRepository {
+internal interface UnitRepository {
 
-    public suspend fun listUnits(): List<UnitEntity>
+    suspend fun listUnits(): List<UnitEntity>
 
-    public suspend fun findUnitById(id: Long): UnitEntity?
+    suspend fun findUnitById(id: Long): UnitEntity?
 
-    public suspend fun createUnit(unit: KUnit)
+    suspend fun createUnit(unit: KUnit)
 
-    public suspend fun updateUnit(
+    suspend fun updateUnit(
         id: Long,
         options: UnitUpdateOptions
     )
 
-    public suspend fun findAuditLogs(unitId: Long): List<AuditLogEntry>?
+    suspend fun findAuditLogs(unitId: Long): List<AuditLogEntry>?
 
-    public suspend fun createAuditLog(auditLogEntry: AuditLogEntry)
+    suspend fun createAuditLog(auditLogEntry: AuditLogEntry)
 }
