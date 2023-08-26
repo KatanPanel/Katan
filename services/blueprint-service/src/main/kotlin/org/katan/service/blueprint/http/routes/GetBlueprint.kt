84130a07f1f3f62c1d1d5b8647c46aad40e580c1
Natan Vieira do Nascimento <itroken99@gmail.com>
2023-08-25 22:46:59 -0300

    Respond ERR1001 when blueprint is not found

--- services/blueprint-service/src/main/kotlin/org/katan/service/blueprint/http/routes/GetBlueprint.kt
+++ services/blueprint-service/src/main/kotlin/org/katan/service/blueprint/http/routes/GetBlueprint.kt
@@ -3,8 +3,12 @@ package org.katan.service.blueprint.http.routes
 import io.ktor.server.resources.get
 import io.ktor.server.routing.Route
 import jakarta.validation.Validator
+import org.katan.http.response.HttpError
 import org.katan.http.response.respond
+import org.katan.http.response.respondError
 import org.katan.http.response.validateOrThrow
+import org.katan.model.toSnowflake
+import org.katan.service.blueprint.BlueprintNotFoundException
 import org.katan.service.blueprint.BlueprintService
 import org.katan.service.blueprint.http.BlueprintRoutes
 import org.katan.service.blueprint.http.dto.BlueprintResponse
@@ -17,7 +21,11 @@ internal fun Route.getBlueprint() {
     get<BlueprintRoutes.ById> { parameters ->
         validator.validateOrThrow(parameters)
 
-        val blueprint = blueprintService.getBlueprint(parameters.blueprintId)
+        val blueprint = try {
+            blueprintService.getBlueprint(parameters.blueprintId.toSnowflake())
+        } catch (_: BlueprintNotFoundException) {
+            respondError(HttpError.UnknownBlueprint)
+        }
         respond(BlueprintResponse(blueprint))
     }
 }
